d8ef798ef2af66f91bf40856654be70e935b1161
nicoly-athayde <nicolyathayde4@gmail.com>
2025-02-26 14:00:23 -0300

    Executando no celular

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Silvinha Health App</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e0e5e5',
+    paddingTop: 50,
+  },
+  titleContainer: {
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
   },
+  title: {
+    color: "#ff0043",
+    fontSize: 28,
+    fontWeight: 'bold',
+  }
+
 });
